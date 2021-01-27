dbf0c5bb95efe9b7c1c5bcf325a9fb9b05bfbe26
Ulises SG <ukisessg@gmail.com>
2021-01-26 18:52:43 -0600

    [ADD] File optimizated to development

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,17 +2,18 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/'
+        publicPath: '/',
     },
     resolve: {
         extensions: ['.js', '.jsx'],
         alias: {
-            '@componets': path.resolve(__dirname, 'src/components')
-        }
+            '@componets': path.resolve(__dirname, 'src/components'),
+        },
     },
     module: {
         rules: [
@@ -20,27 +21,26 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader'
-                }
+                    loader: 'babel-loader',
+                },
             },
             {
                 test: /\.html$/,
-                use: [
-                    { loader: 'html-loader'}
-                ]
-            }
-        ]
+                use: [{ loader: 'html-loader' }],
+            },
+        ],
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: './public/index.html',
-            filename: './index.html'
-        })
+            filename: './index.html',
+        }),
     ],
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
         historyApiFallback: true,
         port: 3005,
+        open: true,
     },
 };
